0ee770bcc940cd09dd58f9668b7ff914f46f0268
Yuvraj Thapa <yuvrajthapa692@gmail.com>
2022-02-05 16:55:42 +0530

    Add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,50 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+        // this.increaseQuantity() = this.increaseQuantity.bind(this)
+    }
+
+    increaseQuantity = () => {
+        // some code
+    }
+
     render() {
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ {fontSize: 25} }>{title}</div>
+                    <div style={ {color: '#777'} }>Rs {price}</div>
+                    <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img 
+                            alt="" 
+                            className='action-icons' 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            onClick={ this.increaseQuantity }>
+                        </img>
+                        <img 
+                            alt="" 
+                            className='action-icons' 
+                            src="https://cdn-icons-png.flaticon.com/512/66/66889.png">
+                        </img>
+                        <img 
+                            alt="" 
+                            className='action-icons' 
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png">
+                        </img>
                     </div>
                 </div>
             </div>
@@ -23,8 +55,8 @@ class CartItem extends React.Component {
 
 const styles = {
     image: {
-        height: 110,
-        width: 110,
+        height: 120,
+        width: 120,
         borderRadius: 4,
         background: '#ccc'
     }
